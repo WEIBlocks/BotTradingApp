c224e79fa6b22860c8a81c2c8f9d98c50388c4e8
Junaid2703 <junaidamjad03@gmail.com>
2026-04-13 20:02:00 +0500

    mac pushs

--- BotTradeApp/index.js
+++ BotTradeApp/index.js
@@ -1,11 +1,14 @@
-import {AppRegistry} from 'react-native';
-import {getMessaging, setBackgroundMessageHandler} from '@react-native-firebase/messaging';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
 // Handle background/quit push notifications (modular API v23+)
-setBackgroundMessageHandler(getMessaging(), async remoteMessage => {
-  console.log('[FCM] Background message:', remoteMessage.notification?.title);
-});
+// Only register on Android - iOS handles background messages natively via AppDelegate
+if (Platform.OS === 'android') {
+  const {getMessaging, setBackgroundMessageHandler} = require('@react-native-firebase/messaging');
+  setBackgroundMessageHandler(getMessaging(), async remoteMessage => {
+    console.log('[FCM] Background message:', remoteMessage.notification?.title);
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
